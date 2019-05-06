40fb620284bdfe0673a71a4a71ca10425e5edb49
Unknown <risahawkeye@mail.ru>
2019-05-06 21:13:59 +0300

    video duration in theme for user

---
+++ getVideosDurationInThemeForUser.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import dateutil.parser
+import time
+
+from queries.getVideoStartAndEnd import ret as videoActionsGenerator
+#videoStateDF=pd.DataFrame(videoActionsGenerator()).sort_values(by=['time'])
+#videoStateDF.to_csv(path_or_buf='video_actions_time.csv',index=False)
+videoStateDF = pd.read_csv('video_actions_time.csv', sep=',', encoding='utf-8')
+
+def getDuration():
+    users=videoStateDF['user_id'].unique()
+    videoDurationDF=[]
+    course_id=videoStateDF['course_id'][0]
+    for user in users:
+        start_time=False
+        timeSequence=videoStateDF.loc[videoStateDF['user_id']==user].sort_values(by='time')
+        video_id=0
+        for row in timeSequence.iterrows():
+            if row[1]['isPlaying']:
+                if video_id!=row[1]['video_id']:
+                    video_id=row[1]['video_id']
+                    theme_id=row[1]['theme_id']
+                start_time=row[1]['time']
+            elif start_time:
+                current_time=row[1]['time']
+                duration=(dateutil.parser.parse(current_time)-dateutil.parser.parse(start_time)).total_seconds()
+                videoDurationDF.append([course_id,theme_id,user,video_id,duration])
+                start_time=False
+    videoDurationDF=pd.DataFrame(videoDurationDF, columns=['course_id','theme_id','user_id','video_id','duration'])
+    videoDurationDF=videoDurationDF.groupby(['course_id','theme_id','user_id'])['duration'].sum()
+    videoDurationDF=videoDurationDF.reset_index()
+    return videoDurationDF
+                
---
+++ queries/getVideoStartAndEnd.py
@@ -0,0 +1,66 @@
+from elasticsearch import Elasticsearch
+import requests
+from  elasticsearch.helpers import scan as scan
+
+es = Elasticsearch([{"host": "localhost", "port": 9200}])
+
+body={    
+    "_source": [
+        "context.user_id",
+        "context.course_id",
+        "referer",
+        "time",
+        "event_type",
+        "str_event" 
+    ],
+    "query": {
+        "bool":{
+            "should":[                
+                {"match_phrase":{
+                    "event_type": "play_video"
+                }},
+                {"match_phrase":{
+                    "event_type": "pause_video"
+                }},
+                {"match_phrase":{
+                    "event_type": "stop_video"
+                }},
+                {"match_phrase":{
+                    "event_type": "page_close"
+                }},
+                {"match_phrase":{
+                    "event_type": "seq_goto"
+                }},
+                {"match_phrase":{
+                    "event_type": "seq_prev"
+                }},
+                {"match_phrase":{
+                    "event_type": "seq_next"
+                }}
+            ]        
+        }
+    }
+}
+
+def ret():
+    for hit in scan(es,
+         index="online_courses", 
+         query=body
+        ):        
+        if hit.get("_source").get("str_event") :
+            isPlaying=hit.get("_source").get("event_type")
+            if isPlaying=="play_video":
+                isPlaying=True
+                video_id=hit.get("_source").get("str_event").split('id\": \"')[-1].split('\"')[0]
+            else:
+                isPlaying=False
+                video_id=''
+            
+            yield {                    
+                "user_id":hit.get("_source").get("context").get("user_id"),                    
+                "course_id":hit.get("_source").get("context").get("course_id"),
+                "time":hit.get("_source").get("time"),
+                "theme_id":hit.get("_source").get("referer").split('courseware/')[-1].split('/')[1],
+                "isPlaying":isPlaying,
+                "video_id":video_id
+            }
