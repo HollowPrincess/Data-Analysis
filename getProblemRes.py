49f5b7aa99e224d88ba662855af8176a9f536de9
Unknown <risahawkeye@mail.ru>
2019-05-22 20:32:29 +0300

    clustering

--- getProblemRes.py
+++ getProblemRes.py
@@ -10,12 +10,12 @@ def grades():
     users=pd.DataFrame(getUser())
     graded=graded.set_index('sub_id').join(users.set_index('sub_id'),how='inner')
     graded=graded.reset_index()
-    graded=graded.groupby(by=['sub_id','user_id','problem_id'])['grade','maxGrade'].sum()
+    graded=graded.groupby(by=['user_id'])['grade','maxGrade'].sum()
     graded=graded.reset_index()
-    graded=graded.loc[:,['user_id','problem_id','grade','maxGrade']]
     
     ungraded=pd.DataFrame(getTestGrade())
-    ungraded=ungraded.groupby(by=['user_id','problem_id'])['grade','maxGrade'].sum()
+    ungraded=ungraded.groupby(by=['user_id'])['grade','maxGrade'].sum()
     ungraded=ungraded.reset_index()
+
     return graded,ungraded
     
--- getUserStudyModel.py
+++ getUserStudyModel.py
@@ -12,9 +12,14 @@ def getModel():
     gradedProbDur=problemsDurationDF.loc[problemsDurationDF['problem_type']=='graded'].drop(['problem_type'], axis=1)
     ungradedProbDur=problemsDurationDF.loc[problemsDurationDF['problem_type']=='ungraded'].drop(['problem_type'], axis=1)
     userModel=gradedProbDur.join(ungradedProbDur.set_index(['course_id','theme_id','user_id']), on=['course_id','theme_id','user_id'],how='outer', lsuffix='_graded', rsuffix='_ungraded')
+    userModel.reset_index()
     
     userModel=userModel.join(messagiesNum.set_index(['course_id','theme_id','user_id']), on=['course_id','theme_id','user_id'],how='outer')
+    userModel.reset_index()
+    
     userModel=userModel.join(videoDurationDF.set_index(['course_id','theme_id','user_id']), on=['course_id','theme_id','user_id'],how='outer')
+    userModel.reset_index()
+    
     userModel=userModel.fillna(0)
     userModel=userModel.rename(index=str, columns={"duration": "video_duration"})
     
